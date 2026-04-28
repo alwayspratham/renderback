2ce169bbfeac253bd3287bea5e3bea6e9afae2bc
alwayspratham <prathamkoparde@gmail.com>
2026-04-28 15:52:35 +0530

    added req.txt

--- models/task.py
+++ models/task.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column,Integer,String,DateTime,Boolean,ForeignKey
 from sqlalchemy.orm import relationship
-from datetime import datetime,UTC
+from datetime import datetime,timezone
 from db.database import Base
 
 class Task(Base):
@@ -10,6 +10,6 @@ class Task(Base):
     description=Column(String(300))
     is_completed=Column(Boolean,default=False)
     owner_id=Column(Integer,ForeignKey("user.id"),nullable=False)
-    created_at=Column(DateTime,default=datetime.now(UTC))
+    created_at=Column(DateTime,default=datetime.now(timezone.utc))
 
     owner=relationship("User",back_populates="tasks")
